6840b11f42401fd2b04e1695a4951768f7aab12a
Nemanja Malesija <malesija13@gmail.com>
2023-02-25 00:19:51 +0100

    Complete nav setup and style

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,7 +3,21 @@ import './nav.css';
 import nemanja from '../utils/images/nemanja.png';
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav className="nav">
+      <div className="image-div">
+        <img className="author-photo" src={nemanja} alt="Nemanja Malesija" />
+        <p className="author-paragraph">Nemanja Malesija</p>
+      </div>
+
+      <ul className="links-list">
+        <li className="list-item">Home</li>
+        <li className="list-item">About</li>
+        <li className="list-item">Projects</li>
+        <li className="contact"></li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Nav;
