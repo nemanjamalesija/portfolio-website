b5ff93a1aea3bbc5e21cb03cdc3a52153d046cdc
Nemanja Malesija <malesija13@gmail.com>
2023-02-24 23:08:21 +0100

    Add about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Nav from './Nav';
+import About from './About';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Nav />
+      <About />
+    </div>
+  );
 }
 
 export default App;
