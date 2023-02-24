2fefca5fc3d9073df0ea5f97a661470c95e72ccb
Nemanja Malesija <malesija13@gmail.com>
2023-02-25 00:36:57 +0100

    Add hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Nav, About, Projects, Contact } from './components';
+import { Nav, About, Projects, Contact, Hero } from './components';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Hero />
       <About />
       <Projects />
       <Contact />
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,5 +2,6 @@ import About from './About';
 import Nav from './Nav';
 import Projects from './Projects';
 import Contact from './Contact';
+import Hero from './Hero';
 
-export { About, Nav, Projects, Contact };
+export { About, Nav, Projects, Contact, Hero };
