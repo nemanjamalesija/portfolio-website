eb62ada3c666223d36798041007c9ecb7ccff6eb
Nemanja Malesija <malesija13@gmail.com>
2023-02-24 23:15:18 +0100

    Add about css

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from 'react';
+import './about.css';
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+  return <div>About</div>;
+};
 
-export default About
+export default About;
