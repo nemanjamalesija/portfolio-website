8a37cbafe3e909d8ada80e4a885c42f27dc661dc
Nemanja Malesija <malesija13@gmail.com>
2023-02-25 01:50:09 +0100

    Complete hero layout

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import './hero.css';
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="section-hero">
+      <div className="container-hero">
+        <p className="hi-paragraph">Hi, my name is</p>
+        <h1 className="big-heading">Nemanja Malesija.</h1>
+        <h2 className="smaller-heading">I turn ideas into code.</h2>
+      </div>
+      <button className="btn btn-projects-hero">Check out my projects!</button>
+    </section>
+  );
 };
 
 export default Hero;
