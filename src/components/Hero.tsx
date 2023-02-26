d8c4dcf60e3db5e43d29b1160881f49f9102422f
Nemanja Malesija <malesija13@gmail.com>
2023-02-26 16:41:38 +0100

    Font family inherit button

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -67,7 +67,7 @@ const Hero = () => {
           <h2 className="smaller-heading">I turn ideas into code.</h2>
           <p className="hero-paragraph">
             I'm a web developer specializing in building interesting digital
-            experiences. Welcome to my page!
+            experiences.
           </p>
         </div>
         <button className="btn-projects-hero" onClick={scrollIntoViewHandler}>
