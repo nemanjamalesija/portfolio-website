b2708e05ed58180bbcc54633d2acca2a6dcdbc78
Nemanja Malesija <malesija13@gmail.com>
2023-02-26 01:15:42 +0100

    Add form structure and style

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,7 +2,49 @@ import React from 'react';
 import './contact.css';
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section className="section section-contact">
+      <div className="container container-contact">
+        <div className="form-container">
+          <h2 className="heading-secondary heading-form">
+            Let's get in touch!
+          </h2>
+          <p className="form-paragraph">
+            Feel free to leave me a message. I'll get back to you as soon as I
+            can!
+          </p>
+
+          <form
+            className="form-contact"
+            action="https://formspree.io/f/xpzedvod"
+            method="POST"
+          >
+            <div>
+              <label>
+                Your email:
+                <input type="email" name="email" />
+              </label>
+            </div>
+
+            <div>
+              <label>
+                Your name:
+                <input type="tet" name="userName" />
+              </label>
+            </div>
+            <div>
+              <label>
+                Your message:
+                <textarea name="message"></textarea>
+              </label>
+            </div>
+
+            <button type="submit">Send</button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
